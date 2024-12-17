86c7e10052a8aeeabccc68cc2b16f491f25e8625
ChatkiAyman <aymanchatki@gmail.com>
2024-12-17 11:33:36 +0100

    Modify file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,25 +8,24 @@ def island_perimeter(grid):
     Calculates the perimeter of the island described in the grid.
 
     Args:
-        grid (list of list of int): A 2D grid where 1 represents land and 0 represents water.
+        grid (list of list of int): A 2D grid where 1 
+        represents land and 0 represents water.
 
     Returns:
         int: The perimeter of the island.
     """
     perimeter = 0
 
-    # Iterate through each cell in the grid
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] == 1:  # If it's land
-                # Check the four sides of the land cell
-                if i == 0 or grid[i - 1][j] == 0:  # North (top side)
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:  # South (bottom side)
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if j == 0 or grid[i][j - 1] == 0:  # West (left side)
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:  # East (right side)
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
 
     return perimeter
